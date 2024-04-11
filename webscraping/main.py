57ee4943ae5a6bf2e9805a761be2054306d00349
Roman Hoehn <rohoehn123@gmail.com>
2024-04-11 22:19:26 +0200

    added logging

--- webscraping/main.py
+++ webscraping/main.py
@@ -3,22 +3,39 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import sqlite3
 import sys
+from os.path import join
+import logging
+
+logger = logging.Logger("webscraping")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(join("webscraping", "webscraping.log"))
+file_handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 url = "https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films"
-db_name = "Movies.db"
+db_name = join("webscraping", "Movies.db")
 table_name = "Top_50"
-csv_path = "top_50_films.csv"
+csv_path = join("webscraping", "top_50_films.csv")
+
 
 resp = requests.get(url)
+logger.info(f"Sent GET Request to {resp.request.url}, HTTP-Status: {resp.status_code}")
+
 
 if resp.status_code != 200:
-    print("Failed to GET HTML")
+    logger.error("Failed to GET HTML")
     sys.exit(-1)
 
 
 soup = BeautifulSoup(resp.text, "html.parser")
 
 tables = soup.find_all("tbody")
+
+if len(tables) == 0:
+    logger.error("Couldnt extract valid table!")
+    sys.exit(-2)
 
 table = tables[0]
 
@@ -36,8 +53,12 @@ for idx, row in enumerate(table.find_all("tr")):
 
 df = pd.DataFrame(data).set_index("Average Rank")
 
+
 with open(csv_path, "w") as f:
     df.to_csv(f)
+    logger.info(f"Written CSV File to {csv_path}")
+
 
 with sqlite3.connect(db_name) as conn:
-    df.to_sql(table_name, conn, if_exists='replace', index=True, index_label="Rank")
+    df.to_sql(table_name, conn, if_exists="replace", index=True, index_label="Rank")
+    logger.info(f"Written Records to Table {table_name} in SQLITE Database {db_name}")
