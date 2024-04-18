0d556b94db9e1f6e573b0413ebcaefa6f97f3f6f
Roman Hoehn <rohoehn123@gmail.com>
2024-04-18 20:03:20 +0200

    works

--- bash_scripting/practice_projects/main.sh
+++ bash_scripting/practice_projects/main.sh
@@ -10,7 +10,7 @@ JUST_TXT="?T" # this forces ASCII plaintext as response instead of ANSI
 
 
 PROJECT_DIR="./bash_scripting/final_project"
-LOG_FILE="$PROJECT_DIR/rx_poc.log"
+LOG_FILE="$PROJECT_DIR/rx_poc.tsv"
 RAW_RESP_FILE="$PROJECT_DIR/${LOCATION}-raw-$TIMESTAMP.txt"
 
 
@@ -23,10 +23,15 @@ curl "$URL$LOCATION$JUST_TXT" > "$RAW_RESP_FILE"
 OBS_LINE=3
 OBS_VAL=$(grep -oE '[0-9]+ °C' "$RAW_RESP_FILE" | cut -d " " -f1 | sed -n "${OBS_LINE}p")
 
-echo $OBS_VAL
-
 FC_LINE=7
 FC_VAL=$(grep -oE '[0-9]+ °C' "$RAW_RESP_FILE" | cut -d " " -f1 | sed -n "${FC_LINE}p")
 
 
-echo $FC_VAL
+YEAR=$(date "+%Y")
+MONTH=$(date "+%m")
+DAY=$(date "+%d")
+HOUR=$(date "+%H")
+
+
+echo -e "$YEAR\t$MONTH\t$DAY\t$HOUR\t$OBS_VAL\t$FC_VAL" >> "$LOG_FILE"
+echo "Successfully logged Temperatures"
